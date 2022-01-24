e25ec974f83db6eda7c0f3b2cece85ff3ae5970c
mthiagoalves <m.thiago.alves@live.com>
2022-01-24 18:57:25 -0300

    ok

--- projeto.js
+++ projeto.js
@@ -1,16 +1,59 @@
 var prompt = require ('prompt-sync')();
 
+console.log(`****Essa história será sobre o personagem que você criar!****\n`)
+
+
 let name = prompt (`Qual seu nome? `)
-    console.log (`\nBem vindo ${name}`);
+    console.log (`\nBem vindo ${name}`);    
+
+    console.log(`\nEm uma vila distante chamada Vinland Sun, vivia uma criança chamada ${name}, 
+filhoª de Thors "O espadachim indomável" e Eldora uma maga especialista em magia elemental.\n
+Frequentemente a vila recebia invasões de demonios que comiam as pessoas, e o pai de ${name} era o chefe
+da guarda da cidade e também era muito forte.\nCerto dia, em treinamento que ${name} fazia todos os dias junto de seu pai, 
+um estranho comerciante passou e contou uma história sobre "As respirações", e falou o nome de um velho que 
+morava no pé da montanha, perto de onde morava e que ele sabia mais sobre esse assunto.
+\nComo ${name} era muito empanhado nos treinamento, decidiu partir em busca do velho para poder aprender
+sobre as respirações.\n`)
+    
 
 let yes = 0;
 let life_boss = 0;
-let body = prompt (`Você conseguiu se fortalecer? `);
-let sensei = prompt (`Você conseguiu um treinador? `);
+
+let sensei = ``;
+    while (sensei != `sim` && sensei != `nao`){
+        sensei.toLocaleLowerCase = prompt(`Você conseguiu encontrar o velho? `);
+        if (sensei == `sim`) {
+            console.log (`Ao encontrar o velho que se chama Izui. Ele pediu para ${name} fazer um teste, se 
+        passasse no teste, Izui o treinaria.\n.\n.\n.`);
+            console.log(`Depois de 2 anos de treinamento, para poder foratalecer o corpo, espirito e as habilidades. ${name}
+        retorna a sua vila, e seu pai a sua espera, está um pouco debilitado pela idade. Pergunta para ele se pode tomar o 
+        posto de guarda da cidade, e fez a seguinte pergunta:\n`)
+        } else {
+            console.log(`\nComo ${name} não encontrou o velho, teve que pesquisar e treinar sozinho.
+        Demorou para se fortalecer evoltou para casa, não tão diferente de quando saiu, e foi a uma conversa com seu velho pai.\n`);
+        }
+}   
+/*
+console.log(`Depois de 2 anos de treinamento, para poder foratalecer o corpo, espirito e as habilidades. ${name}
+retorna a sua vila, e seu pai a sua espera, está um pouco debilitado pela idade. Pergunta para ele se pode tomar o 
+posto de guarda da cidade, e fez a seguinte pergunta:\n`)
+*/
+let body = ``;
+    while (body != `sim` || body != `nao`){
+        body.toLocaleLowerCase = prompt(`Digite "sim" ou "não". Você conseguiu se fortalecer? `);
+    } if(body == `sim`) {
+        console.log (`\nJá que ${name} estava bem fortalecido, Thors seu pai, mandou ele em busca de um velho amigo,
+um ferreiro, para poder fazer uma espada com um ferro especial, para poder acabar com as ameaças da cidade.`);
+} else{
+    console.log (`\nComo ${name} não conseguiu se fortalecer, a cidade foi invadida por demonios na noite seguinte,
+e sem sem pai, e ninguem a altura para proteger a cidade, todos morreram.`);
+    } 
+        
+    
 let blacksmith = prompt (`Você encontrou um ferreiro para fazer sua espada? `);
 let breathing = prompt (`Você conseguiu dominar as respirações? `);
 let strangh = prompt (`Conseguiu acumuluar forças caçando demônios? `);
-
+/*
 //PARTE DE VERIFICAÇÃO PARA AVANÇAR NA HISTÓRIA 
 
 if (body.toLocaleLowerCase() == `sim`){
@@ -97,5 +140,5 @@ if (life_boss == 50000 || life_boss == 1000) {
 
 
 
-
+*/
 
