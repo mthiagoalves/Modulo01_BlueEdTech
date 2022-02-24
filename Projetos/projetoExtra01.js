651328fb2a76442c9e6fd84a1d36a2cc62004763
mthiagoalves <m.thiago.alves@live.com>
2022-02-24 15:06:35 -0300

    projeto

---
+++ Projetos/projetoExtra01.js
@@ -0,0 +1,242 @@
+const prompt = require("prompt-sync")();
+const { Console } = require("console");
+const { Transform } = require("stream");
+
+//PRINCIPAIS VARIAVEIS
+
+let userVote;
+let exit;
+let userName;
+let userAge;
+let vote1 = 0;
+let vote2 = 0;
+let vote3 = 0;
+let voteNull = 0;
+let voteBlank = 0;
+let sum;
+let rest;
+let cpf;
+
+//ARROW FUNCTION PARA CONTAGEM DOS VOTOS E EXIBIÇÃO DO RESULTADO
+displayResult = () => {
+  console.log(
+    `\nO candidato \x1b[33m${candidates[0]["Candidato"]}\x1b[0m teve \x1b[31m${vote1}\x1b[0m votos.`
+  );
+  console.log(
+    `O candidato \x1b[32m${candidates[1]["Candidato"]}\x1b[0m teve \x1b[31m${vote2}\x1b[0m votos.`
+  );
+  console.log(
+    `O candidato \x1b[34m${candidates[2]["Candidato"]}\x1b[0m teve \x1b[31m${vote3}\x1b[0m votos.`
+  );
+  console.log(
+    `O candidato ${candidates[3]["Candidato"]} teve \x1b[31m${voteNull}\x1b[0m votos.`
+  );
+  console.log(
+    `O candidato ${candidates[4]["Candidato"]} teve \x1b[31m${voteBlank}\x1b[0m votos.`
+  );
+
+  if (vote1 > vote2 && vote1 > vote3) {
+    console.log(
+      `\nO candidato \x1b[33m${candidates[0]["Candidato"]}\x1b[0m ganhou a eleição.`
+    );
+  } else if (vote2 > vote1 && vote2 > vote3) {
+    console.log(
+      `\nO candidato \x1b[32m${candidates[1]["Candidato"]}\x1b[0m ganhou a eleição.`
+    );
+  } else if (vote3 > vote1 && vote3 > vote2) {
+    console.log(
+      `\nO candidato \x1b[34m${candidates[2]["Candidato"]}\x1b[0m ganhou a eleição.`
+    );
+  } else {
+    console.log(`\nNão teve ganhadores nessa eleição.`);
+  }
+};
+
+//ARROW FUNCTION PARA CONTAR TEMPO DENTRO DO PROGRAMA
+time = (ms) => {
+  let contar = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - contar > ms) {
+      break;
+    }
+  }
+};
+
+//ARROW FUNCTION PARA PERSONALIZAR E TABELAR UM OBJETO
+table = (input) => {
+  const ts = new Transform({
+    transform(chunk, enc, cb) {
+      cb(null, chunk);
+    },
+  });
+  const logger = new Console({ stdout: ts });
+  logger.table(input);
+  const table = (ts.read() || "").toString();
+  let result = "";
+  for (let row of table.split(/[\r\n]+/)) {
+    let r = row.replace(/[^┬]*┬/, "┌");
+    r = r.replace(/^├─*┼/, "├");
+    r = r.replace(/│[^│]*/, "");
+    r = r.replace(/^└─*┴/, "└");
+    r = r.replace(/'/g, " ");
+    result += `${r}\n`;
+  }
+  console.log(result);
+};
+
+//ARROW FUNCTION PARA VALIDAÇÃO SE O USUARIO ESTÁ APTO PARA VOTAÇÃO.
+ageValidate = (age) => {
+  if (!isNaN(age) && age >= 16 && age < 18 && age % 1 == 0 && age.length != 0) {
+    console.log(`\n${userName} você tem ${age} anos, seu voto é opcional.\n`);
+  } else if (
+    !isNaN(age) &&
+    age >= 18 &&
+    age < 70 &&
+    age % 1 == 0 &&
+    age.length != 0
+  ) {
+    console.log(
+      `\n${userName} você tem ${age} anos, seu voto é obrigatório.\n`
+    );
+  } else if (!isNaN(age) && age >= 70 && age % 1 == 0 && age.length != 0) {
+    console.log(`\n${userName} você tem ${age} anos, seu voto é opcional.\n`);
+  } else {
+    console.log(`\nVocê não está apto para votação.\n`);
+  }
+  return age;
+};
+
+//ARROW FUNCTION PARA CONTAGEM DOS VOTOS
+voteCount = (userVote) => {
+  if (userVote == `1`) {
+    console.log(
+      `O candidato escolhido foi a \x1b[33m${candidates[0]["Candidato"]}\x1b[0m.`
+    );
+    vote1++;
+  } else if (userVote == `2`) {
+    vote2++;
+    console.log(
+      `O candidato escolhido foi o \x1b[32m${candidates[1]["Candidato"]}\x1b[0m. `
+    );
+  } else if (userVote == `3`) {
+    vote3++;
+    console.log(
+      `O candidato escolhido foi o \x1b[34m${candidates[2]["Candidato"]}\x1b[0m`
+    );
+  } else if (userVote == `4`) {
+    voteNull++;
+    console.log(`Seu voto foi computado ${candidates[3]["Candidato"]} `);
+  } else if (userVote == `5`) {
+    voteBlank++;
+    console.log(`Seu voto foi em ${candidates[4]["Candidato"]}`);
+  } else {
+    console.log(`Numero inválido para votação. Escolha de 1 à 5.`);
+  }
+};
+
+//ARROW FUNCTION PARA VALIDAÇÃO DO CPF DO USUARIO.
+
+testCpf = (cpf) => {
+  sum = 0;
+  if (cpf == "00000000000") return `CPF invalido`;
+
+  for (i = 1; i <= 9; i++) sum = sum + cpf.substring(i - 1, i) * (11 - i);
+  rest = (sum * 10) % 11;
+
+  if (rest == 10 || rest == 11) rest = 0;
+  if (rest != cpf.substring(9, 10)) return `CPF invalido`;
+
+  sum = 0;
+  for (i = 1; i <= 10; i++) sum = sum + cpf.substring(i - 1, i) * (12 - i);
+  rest = (sum * 10) % 11;
+
+  if (rest == 10 || rest == 11) rest = 0;
+  if (rest != cpf.substring(10, 11)) return `CPF invalido`;
+  return `CPF validado`;
+};
+
+//ARRAY COM AS ESCOLHAS DO USUARIO.
+const candidates = [
+  { Opção: 1, Candidato: "Camila" },
+  { Opção: 2, Candidato: "Kellbber" },
+  { Opção: 3, Candidato: "Thiago" },
+  { Opção: 4, Candidato: "Voto Nulo" },
+  { Opção: 5, Candidato: "Voto em Branco" },
+];
+
+//*************************************** INICIO ***************************************
+
+//ENTRADA DO CPF DO USUARIO VALIDANDO COM A ARROW FUNCTION
+do {
+  cpf = prompt(`Digite seu CPF: `);
+  console.log(`Consultando CPF. Aguarde.`);
+  time(1000);
+  console.log(`.`);
+  time(1500);
+  console.log(`.`);
+  time(1500);
+  console.log(`.`);
+  time(1500);
+  console.log(testCpf(cpf));
+  time(1700);
+  console.clear();
+} while (testCpf(cpf) == `CPF invalido`);
+
+//ENTRADA E VALIDAÇÃO DO NOME DO USUARIO
+userName = prompt(`Digite seu nome: `);
+while (!isNaN(userName)) {
+  console.clear();
+  console.log(`!!!ATENÇÃO!!!`);
+  userName = prompt(`Digite um nome válido: `);
+  console.clear();
+}
+
+//ENTRADA E VALIDAÇÃO DO IDADE DO USUARIO
+while (true) {
+  userAge = +prompt(`Digite sua idade:  `);
+  if (
+    !isNaN(userAge) &&
+    userAge > 0 &&
+    userAge % 1 == 0 &&
+    userAge.length != 0
+  ) {
+    break;
+  }
+  console.clear();
+  console.log(`Digite uma idade válida.`);
+}
+
+// ENTRADA E VALIDAÇÃO DA IDADE DO USUARIO
+ageValidate(userAge);
+
+if (userAge > 15) {
+  console.log(`As opções de voto são: `);
+
+  table(candidates);
+
+  //CONTAGEM E VALIDAÇÃO DOS VOTOS
+
+  while (true) {
+    userVote = +prompt(`Digite seu voto de acordo com a opção desejada: `);
+
+    voteCount(userVote);
+
+    console.log();
+
+    //CONDIÇÃO PARA CONTINUAR A VOTAÇÃO
+
+    exit = prompt(`Deseja continuar a votação? `).toLowerCase;
+
+    if (exit == `não` || exit == `n` || exit == `nao`) {
+    }
+    console.clear();
+    break;
+  }
+
+  displayResult();
+}
+
+//CASO O USUARIO SEJA MENOR QUE 16 ANOS. AUTOMÁTICAMENTE REPROVA-O.
+else {
+  console.log(`Volte quando sua idade for maior ou igual 16 para votar.`);
+}
