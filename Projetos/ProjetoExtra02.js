90e3f0ce3c6f13201b95b650908419556fd75efe
mthiagoalves <m.thiago.alves@live.com>
2022-02-28 20:22:10 -0300

    projeto

--- Projetos/ProjetoExtra02.js
+++ Projetos/ProjetoExtra02.js
@@ -1,4 +1,4 @@
-const prompt = require ('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 let numGame;
 let numPlayers;
@@ -6,37 +6,43 @@ const choice = [1, 2, 3, 4, 5, 6];
 const players = [];
 let choiseRandom;
 let resultRandon;
+let continuar;
 
 while (true) {
-    numPlayers = +prompt(`Digite o número de jogadores vão jogar: `);
-    if (
-        !isNaN(numPlayers) &&
-        numPlayers > 0 &&
-        numPlayers % 1 == 0 &&
-        numPlayers.length != 0
-      ) {
-        break;
-      }
-  }
 
+  numPlayers = +prompt(`Digite o número de jogadores vão jogar: `);
+
+  if (
+    !isNaN(numPlayers) &&
+    numPlayers > 0 &&
+    numPlayers % 1 == 0 &&
+    numPlayers.length != 0
+  ) {
+    break;
+  }
+}
 
 while (true) {
-    numGame = +prompt(`Digite o número de rodadas você deseja jogar: `);
-    if (
-        !isNaN(numGame) &&
-        numGame > 0 &&
-        numGame % 1 == 0 &&
-        numGame.length != 0
-      ) {
-        break;
-      }
+
+  numGame = +prompt(`Digite o número de rodadas você deseja jogar: `);
+
+  if (
+    !isNaN(numGame) &&
+    numGame > 0 &&
+    numGame % 1 == 0 &&
+    numGame.length != 0
+  ) {
+    break;
   }
+}
 
-  for (i = 0; i < numGame; i++) {
+continuar = prompt (`Aperte ENTER para continuar.\n`);
 
-    choiseRandom = [Math.floor(Math.random() * choice.length)];
-    resultRandon = choice[choiseRandom];
-    console.log(resultRandon)
-  }
+for (i = 0; i < numGame; i++) {
 
- 
+  choiseRandom = [Math.floor(Math.random() * choice.length)];
+  resultRandon = choice[choiseRandom];
+  console.log(resultRandon);
+
+}
+
