5fffac8ca9b0a849234da75068b3c383d39657c5
mthiagoalves <m.thiago.alves@live.com>
2022-03-02 09:39:46 -0300

    projeto

---
+++ Projetos/ProjetoExtra02.js
@@ -0,0 +1,48 @@
+const prompt = require("prompt-sync")();
+
+let numGame;
+let numPlayers;
+const choice = [1, 2, 3, 4, 5, 6];
+const players = [];
+let choiseRandom;
+let resultRandon;
+let continuar;
+
+while (true) {
+
+  numPlayers = +prompt(`Digite o número de jogadores vão jogar: `);
+
+  if (
+    !isNaN(numPlayers) &&
+    numPlayers > 0 &&
+    numPlayers % 1 == 0 &&
+    numPlayers.length != 0
+  ) {
+    break;
+  }
+}
+
+while (true) {
+
+  numGame = +prompt(`Digite o número de rodadas você deseja jogar: `);
+
+  if (
+    !isNaN(numGame) &&
+    numGame > 0 &&
+    numGame % 1 == 0 &&
+    numGame.length != 0
+  ) {
+    break;
+  }
+}
+
+continuar = prompt (`Aperte ENTER para continuar.\n`);
+
+for (i = 0; i < numGame; i++) {
+
+  choiseRandom = [Math.floor(Math.random() * choice.length)];
+  resultRandon = choice[choiseRandom];
+  console.log(resultRandon);
+
+}
+
